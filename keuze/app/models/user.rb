ba6627eb1b671caf52a3f8cacab7d3bb867f94dd
WinterOrb <riporbofwinter@gmail.com>
2018-12-04 03:05:18 +0100

    profile page mostly designed, upploading an img as file to db done, sign up inprogress(needs failsaves)

--- keuze/app/models/user.rb
+++ keuze/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_one_attached :image
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, length: { maximum: 50 }
 	#REGEX matching language for more look at http://www.rubular.com/

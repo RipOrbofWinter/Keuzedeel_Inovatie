63bab96b6cb674083b3653e4a9ad0432a4a96886
WinterOrb <riporbofwinter@gmail.com>
2018-12-05 04:23:45 +0100

    small update to keep master up to date

--- keuze/app/controllers/users_controller.rb
+++ keuze/app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  	def show
+    	@user = User.find(params[:id])
+  	end
 
-  end
-
-  def user_params
+  	def user_params
       	params.require(:user).permit(:image)
-  end
+  	end
 
-  def new
-  	@user = User.new
+	def new
+  		@user = User.new
+  	end
+  	
+    def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
